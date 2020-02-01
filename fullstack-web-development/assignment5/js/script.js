2cf108bb576280231720c11a351da1309b176e7f
unknown <rresh@thinkramarag.redmond.corp.microsoft.com>
2020-01-31 23:47:02 -0800

    assignment5

--- fullstack-web-development/assignment5/js/script.js
+++ fullstack-web-development/assignment5/js/script.js
@@ -103,7 +103,7 @@ function buildAndShowHomeHTML (categories) {
       
       // variable's name implies it expects.
       var chosenCategory=chooseRandomCategory(categories);
-      var chosenCategoryShortName = "" +chosenCategory.short_name;
+      var chosenCategoryShortName = "'" + chosenCategory.short_name + "'";
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
